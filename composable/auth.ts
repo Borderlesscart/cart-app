f63a77c428b4d94d26c6d0bc83171177213a4400
Ekoh Franklin <ekohfranklin@gmail.com>
2024-05-24 00:24:55 +0100

    use of composer api the proper way

--- composable/auth.ts
+++ composable/auth.ts
@@ -1,47 +1,56 @@
 import { baseApi } from "~/composable/endpoints";
 import { useApi } from "./apiWrapper";
 
-const API = useApi()
+
 
 export const loginUser = async (data: any): Promise<any> => {
+    const API = useApi()
    return await API.post(baseApi.auth.login(), data)
 }
 
 export const registerUser = async (data: any): Promise<any> => {
+    const API = useApi()
     return await API.post(baseApi.auth.register(), data)
 }
 
 export const sendOtp = async (data: {phone: string}): Promise<any> => {
+    const API = useApi()
     const phone = useCookie('phone')
     phone.value = data.phone
     return await API.post(baseApi.auth.otp(), data)
 }
 
 export const verifyOtp = async (data: {'phone': String, code: Number}): Promise<any> => {
+    const API = useApi()
     return await API.post(baseApi.auth.verify_otp(), data)
 }
 
 export const sendPasswordResetOtp = async (data: {phone: string}): Promise<any> => {
+    const API = useApi()
     const phone = useCookie('phone')
     phone.value = data.phone
     return await API.post(baseApi.auth.forgot_password(), data)
 }
 
 export const sendPasswordResetOtpEmail = async (data: {email: string}): Promise<any> => {
+    const API = useApi()
     const email = useCookie('email')
     email.value = data.email
     return await API.post(baseApi.auth.forgot_password_email(), data)
 }
 
 export const verifyPasswordReset = async (data: {phone: string, code: number}): Promise<any> => {
+    const API = useApi()
     return await API.post(baseApi.auth.verify_password_reset(), data)
 }
 
 export const verifyPasswordResetEmail = async (data: {email: string, code: number}): Promise<any> => {
+    const API = useApi()
     return await API.post(baseApi.auth.verify_password_reset_email(), data)
 }
 
 export const updatePassword = async (data: {password: string, confirm_password: string}): Promise<any> => {
+    const API = useApi()
     const response = await API.put(baseApi.auth.update_password(), data)
     return response
 }
